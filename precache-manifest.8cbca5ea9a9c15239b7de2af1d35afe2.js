6d0f729b64eeb52e06001dd0dac9d32ffbc405e3
julienbigras <julien.bigras@hotmail.com>
2019-06-07 03:05:18 -0400

    Updates

---
+++ precache-manifest.8cbca5ea9a9c15239b7de2af1d35afe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edff06e19199314cce8214b09fc9752",
+    "url": "/show-and-tell/index.html"
+  },
+  {
+    "revision": "fd2e8bc097773f5ff5bb",
+    "url": "/show-and-tell/static/css/main.313318f8.chunk.css"
+  },
+  {
+    "revision": "1c7d0b031590dcbffb89",
+    "url": "/show-and-tell/static/js/2.aa94110e.chunk.js"
+  },
+  {
+    "revision": "fd2e8bc097773f5ff5bb",
+    "url": "/show-and-tell/static/js/main.15852980.chunk.js"
+  },
+  {
+    "revision": "258c05b93f578b815f2d",
+    "url": "/show-and-tell/static/js/runtime~main.6d3d50ea.js"
+  }
+]);
